134f7ed03e1864c821126eb9a22c0f35d0cd0ba9
Ankita Soni <38311448+ASoni01@users.noreply.github.com>
2022-12-20 13:42:03 +0530

    router-dom

---
+++ src/ActivationPage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const ActivationPage =() =>{
+    return(
+        <div>Activation Link</div>
+    )
+}
+
+export default ActivationPage;
--- src/App.js
+++ src/App.js
@@ -2,13 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 import Form from './Form.js';
 import Header from './Header.js';
+import {
+  BrowserRouter,
+  Route,
+  Routes
+} from 'react-router-dom';
+import ActivationPage from './ActivationPage.js';
 
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <Form />
+      <BrowserRouter>
+           <Routes>
+       <Route
+            exact
+            path="/"
+            element={<Form />}
+          />
+          <Route exact path="/activationLink" element={<ActivationPage />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
