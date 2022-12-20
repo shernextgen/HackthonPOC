def87c4111a2e85884645d01882e0d4eb728bf8a
Aditi Srivastava <99032318+aditi-srvstv@users.noreply.github.com>
2022-12-20 15:29:01 +0530

    activation page

--- src/ActivationPage.js
+++ src/ActivationPage.js
@@ -1,8 +1,89 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const ActivationPage =() =>{
-    return(
-        <div>Activation Link</div>
+const ActivationPage = () => {
+    const [otp, setOtp] = useState({ value: '', otp1: "", otp2: "", otp3: "", otp4: "" });
+
+    const handleChange = (value1, event) => {
+        // setOtp({ [value1]: event.target.value });
+        setOtp({
+            ...otp,
+            [value1]: event.target.value
+          })
+    }
+
+    const handleSubmit = (event) => {
+        const data = new FormData(event.target);
+        console.log("otp-", otp);
+        event.preventDefault();
+    }
+
+    const inputfocus = (elmnt) => {
+        if (elmnt.key === "Delete" || elmnt.key === "Backspace") {
+            const next = elmnt.target.tabIndex - 2;
+            if (next > -1) {
+
+                elmnt.target.form.elements[next].focus()
+            }
+        }
+        else {
+            const next = elmnt.target.tabIndex;
+            if (next < 5) {
+                elmnt.target.form.elements[next].focus()
+            }
+        }
+
+    }
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div className="otpContainer">
+                    <input
+                        name="otp1"
+                        type="text"
+                        autoComplete="off"
+                        className="otpInput"
+                        value={otp.otp1}
+                        onChange={e => handleChange("otp1", e)}
+                        tabIndex="1" maxLength="1" onKeyUp={e => inputfocus(e)}
+
+                    />
+                    <input
+                        name="otp2"
+                        type="text"
+                        autoComplete="off"
+                        className="otpInput"
+                        value={otp.otp2}
+                        onChange={e => handleChange("otp2", e)}
+                        tabIndex="2" maxLength="1" onKeyUp={e => inputfocus(e)}
+
+                    />
+                    <input
+                        name="otp3"
+                        type="text"
+                        autoComplete="off"
+                        className="otpInput"
+                        value={otp.otp3}
+                        onChange={e => handleChange("otp3", e)}
+                        tabIndex="3" maxLength="1" onKeyUp={e => inputfocus(e)}
+
+                    />
+                    <input
+                        name="otp4"
+                        type="text"
+                        autoComplete="off"
+                        className="otpInput"
+                        value={otp.otp4}
+                        onChange={e => handleChange("otp4", e)}
+                        tabIndex="4" maxLength="1" onKeyUp={e => inputfocus(e)}
+                    />
+                </div>
+                <button className="primary" type="submit">
+                    Submit
+                </button>
+            </form>
+        </div>
+
     )
 }
 
